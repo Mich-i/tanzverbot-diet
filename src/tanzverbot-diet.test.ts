6440b3d09da3cc0ffb06449c67d2ff505744cdca
Michael Stutz <michael_stutz@sluz.ch>
2025-05-16 08:44:05 +0200

    Testfall 1 geschrieben

--- src/tanzverbot-diet.test.ts
+++ src/tanzverbot-diet.test.ts
@@ -5,4 +5,7 @@ test("Tanzverbot Diet", () => {
 });
 
 
-// testtesttest
+test("Tanzverbot Diet: 74kg → 100kg, 186cm, 38y, männlich", () => {
+  const days = calcDateOnDiet(74, 100, 1.86, 38, Sex.Male);
+  expect(days).toBe(64); // exakter Wert nach Berechnung
+});
